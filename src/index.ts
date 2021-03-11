74cea8f70460f0392d79ad18545068d3da7cc792
qingunaghui <1348975357@qq.com>
2021-03-11 22:19:03 +0800

    初始化项目

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+const ddd = 444;
+
+// interface Dss {
+//   name: string;
+
+//   pwd: string;
+// }
+const baddddd: object = {
+  add: 3,
+
+  ddd
+};
+
+console.log(baddddd);
+
+function a(): void {
+  console.log(233);
+}
+
+a();
